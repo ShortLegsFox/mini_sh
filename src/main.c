d4a4a28ec1ec322fe8f776668122e64076447c67
ShortLegsFox <mister.dusbin777@gmail.com>
2025-01-05 14:09:43 +0100

    feat: removing old structure from skeleton and starting from base (minishell prompt and exit command)

--- src/main.c
+++ src/main.c
@@ -11,158 +11,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <getopt.h>
 #include <main.h>
 
-
 /**
- * Procedure which displays binary usage
- * by printing on stdout all available options
+ * Display prompt
  *
- * \return void
+ * void method
+ * Showing the "mini_sh$ " prompt in terminal with stdout to retrieve command input from the user
  */
-void print_usage(char *bin_name) {
-    dprintf(1, "USAGE: %s %s\n\n%s\n", bin_name, USAGE_SYNTAX, USAGE_PARAMS);
+void display_prompt() {
+    printf("mini_sh$ ");
+    fflush(stdout);
 }
 
-
 /**
- * Procedure checks if variable must be free
- * (check: ptr != NULL)
- *
- * \param void* to_free pointer to an allocated mem
- * \see man 3 free
- * \return void
- */
-void free_if_needed(void *to_free) {
-    if (to_free != NULL) free(to_free);
-}
-
-
-/**
- *
- * \see man 3 strndup
- * \see man 3 perror
- * \return
- */
-char *dup_optarg_str() {
-    char *str = NULL;
-
-    if (optarg != NULL) {
-        str = strndup(optarg, MAX_PATH_LENGTH);
-
-        // Checking if ERRNO is set
-        if (str == NULL)
-            perror(strerror(errno));
-    }
-
-    return str;
-}
-
-
-/**
- * Binary options declaration
- * (must end with {0,0,0,0})
- *
- * \see man 3 getopt_long or getopt
- * \see struct option definition
- */
-static struct option binary_opts[] =
-{
-    {"help", no_argument, 0, 'h'},
-    {"verbose", no_argument, 0, 'v'},
-    {"input", required_argument, 0, 'i'},
-    {"output", required_argument, 0, 'o'},
-    {0, 0, 0, 0}
-};
-
-/**
- * Binary options string
- * (linked to optionn declaration)
- *
- * \see man 3 getopt_long or getopt
- */
-const char *binary_optstr = "hvi:o:";
-
-
-/**
- * Binary main loop
+ * Shell main loop
  *
  * \return 1 if it exit successfully
  */
 int main(int argc, char **argv) {
-    /**
-     * Binary variables
-     * (could be defined in a structure)
-     */
-    short int is_verbose_mode = 0;
-    char *bin_input_param = NULL;
-    char *bin_output_param = NULL;
+    char command[MAX_LENGTH];
 
-    // Parsing options
-    int opt = -1;
-    int opt_idx = -1;
+    while (1) {
+        display_prompt();
 
-    while ((opt = getopt_long(argc, argv, binary_optstr, binary_opts, &opt_idx)) != -1) {
-        switch (opt) {
-            case 'i':
-                //input param
-                if (optarg) {
-                    bin_input_param = dup_optarg_str();
-                }
-                break;
-            case 'o':
-                //output param
-                if (optarg) {
-                    bin_output_param = dup_optarg_str();
-                }
-                break;
-            case 'v':
-                //verbose mode
-                is_verbose_mode = 1;
-                break;
-            case 'h':
-                print_usage(argv[0]);
+        if (fgets(command, MAX_LENGTH, stdin) == NULL) {
+            printf("Exiting mini_sh, Goodbye !");
+            break;
+        }
 
-                free_if_needed(bin_input_param);
-                free_if_needed(bin_output_param);
+        printf("You entered: %s\n", command);
 
-                exit(EXIT_SUCCESS);
-            default:
-                break;
+        command[strcspn(command, "\n")] = '\0';
+
+        if (strcmp(command, "exit") == 0) {
+            printf("Exiting mini_sh, Goodbye !");
+            break;
         }
     }
-
-    /**
-     * Checking binary requirements
-     * (could defined in a separate function)
-     */
-    if (bin_input_param == NULL || bin_output_param == NULL) {
-        dprintf(STDERR, "Bad usage! See HELP [--help|-h]\n");
-
-        // Freeing allocated data
-        free_if_needed(bin_input_param);
-        free_if_needed(bin_output_param);
-        // Exiting with a failure ERROR CODE (== 1)
-        exit(EXIT_FAILURE);
-    }
-
-
-    // Printing params
-    dprintf(1, "** PARAMS **\n%-8s: %s\n%-8s: %s\n%-8s: %d\n",
-            "input", bin_input_param,
-            "output", bin_output_param,
-            "verbose", is_verbose_mode);
-
-    // Business logic must be implemented at this point
-
-    /* LOREM IPSUM DOT SIR AMET */
-
-
-    // Freeing allocated data
-    free_if_needed(bin_input_param);
-    free_if_needed(bin_output_param);
-
-
     return EXIT_SUCCESS;
 }
