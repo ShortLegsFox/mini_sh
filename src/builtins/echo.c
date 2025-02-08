5c3c35a43bbe09bd198ff6c814fa611bec91fdb1
ShortLegsFox <mister.dusbin777@gmail.com>
2025-02-08 12:14:44 +0100

    feat: echo builtin

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -9,10 +9,28 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include "builtins.h"
 
+
 int builtin_echo(char **args) {
-    // To be implemented
+    for (int i = 1; args[i] != NULL; i++) {
+        char *arg = args[i];
+        int len = strlen(arg);
+
+        // If the first and last characters are quotes, print inside part only
+        if ((arg[0] == '"' || arg[0] == '\'') && arg[len - 1] == arg[0] && len > 1) {
+            fwrite(arg + 1, 1, len - 2, stdout);
+        } else {
+            printf("%s", arg);
+        }
+
+        if (args[i + 1] != NULL) {
+            printf(" ");
+        } else {
+            printf("\n");
+        }
+    }
     return 0;
 }
